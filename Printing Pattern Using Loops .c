22dd8873f455004c30b4e35bf9e8de8b72fa66f0
ChurchYan <hjshhb@outlook.com>
2021-03-28 21:07:32 +0200

    Add files via upload

---
+++ Printing Pattern Using Loops .c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int max(int a,int b){
+    if (a>b){
+        return a;
+    } else{
+        return b;
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int n1 = n-1;
+    int n2 = 2*n-2;
+    for (int x = 0; x <= n2; ++x) {
+        for (int y = 0; y <= n2; ++y) {
+            int absx = abs(x-n1);
+            int absy = abs(y-n1);
+            int max_num = max(absx,absy);
+            printf("%d  ",1+max_num);
+        }
+        printf("\n");
+    }
+    return 0;
+}
